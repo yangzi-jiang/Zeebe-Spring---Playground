480f755819c0254606921fa0f95f6e460c298259
Yangzi Jiang <yangzi.jiang@payzer.com>
2021-05-20 12:27:32 -0400

    changes

--- src/main/java/io/camunda/cloudstarter/CloudStarterApplication.java
+++ src/main/java/io/camunda/cloudstarter/CloudStarterApplication.java
@@ -43,51 +43,51 @@ public class CloudStarterApplication {
 		Topology topology = client.newTopologyRequest().send().join();
 		return topology.toString();
 	}
-
-	@Scheduled(cron = "${cron.expression}", zone = "${cron.zone}")
-	public void startPricingWorkflow() throws InterruptedException {
-		if (!client.isRunning()) {
-			return;
-		}
-
-		// Set time as the UUID for BPMN process ID
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
-		LocalDateTime now = LocalDateTime.now();
-
-		final ProcessInstanceResult event =
-				client
-						.newCreateInstanceCommand()
-						.bpmnProcessId("{\"time\": \"" + dtf.format(now) + "\"}")
-						.latestVersion()
-						.withResult()
-						.send()
-						.join();
-
-		logger.info("started instance for workflowKey='{}', bpmnProcessId='{}', version='{}' with workflowInstanceKey='{}'",
-				event.getProcessDefinitionKey(), event.getBpmnProcessId(), event.getVersion(), event.getProcessInstanceKey());
-
-		// Prepare variable for data flow in Zeebe processes
-		final FetchStatus fetchStatus = new FetchStatus();
-		fetchStatus.setFetchStatusID(UUID.randomUUID());
-		fetchStatus.setCustomerName("yangzi");
-		fetchStatus.setBusinessLabel("yangzi's workflow #1");
-		fetchStatus.setCatalogFileName("N/A");
-
-		client
-				.newPublishMessageCommand()
-				.messageName("fetching").correlationKey("correlationValue")
-				.variables(fetchStatus)
-				.send().join();
-
-
-		TimeUnit.SECONDS.sleep(5);
-
-		// Send message to the Message Receive Task in the Event Sub Process
-		fetchStatus.setFetchComplete(Boolean.TRUE);
-		client
-				.newPublishMessageCommand()
-				.messageName("Pricing Integration Running").correlationKey(fetchStatus.getBusinessLabel())
-				.variables(fetchStatus)
-				.send().join();
-	}
+//
+//	@Scheduled(cron = "${cron.expression}", zone = "${cron.zone}")
+//	public void startPricingWorkflow() throws InterruptedException {
+//		if (!client.isRunning()) {
+//			return;
+//		}
+//
+//		// Set time as the UUID for BPMN process ID
+//		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
+//		LocalDateTime now = LocalDateTime.now();
+//
+//		final ProcessInstanceResult event =
+//				client
+//						.newCreateInstanceCommand()
+//						.bpmnProcessId("{\"time\": \"" + dtf.format(now) + "\"}")
+//						.latestVersion()
+//						.withResult()
+//						.send()
+//						.join();
+//
+//		logger.info("started instance for workflowKey='{}', bpmnProcessId='{}', version='{}' with workflowInstanceKey='{}'",
+//				event.getProcessDefinitionKey(), event.getBpmnProcessId(), event.getVersion(), event.getProcessInstanceKey());
+//
+//		// Prepare variable for data flow in Zeebe processes
+//		final FetchStatus fetchStatus = new FetchStatus();
+//		fetchStatus.setFetchStatusID(UUID.randomUUID());
+//		fetchStatus.setCustomerName("yangzi");
+//		fetchStatus.setBusinessLabel("yangzi's workflow #1");
+//		fetchStatus.setCatalogFileName("N/A");
+//
+//		client
+//				.newPublishMessageCommand()
+//				.messageName("fetching").correlationKey("correlationValue")
+//				.variables(fetchStatus)
+//				.send().join();
+//
+//
+//		TimeUnit.SECONDS.sleep(5);
+//
+//		// Send message to the Message Receive Task in the Event Sub Process
+//		fetchStatus.setFetchComplete(Boolean.TRUE);
+//		client
+//				.newPublishMessageCommand()
+//				.messageName("Pricing Integration Running").correlationKey(fetchStatus.getBusinessLabel())
+//				.variables(fetchStatus)
+//				.send().join();
+//	}
 }
