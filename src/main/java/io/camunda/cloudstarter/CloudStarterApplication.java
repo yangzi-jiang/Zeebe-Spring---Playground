219f0d0e4780bee71b7f535b31574d17ca18d3fe
Yangzi Jiang <yangzi.jiang@payzer.com>
2021-05-20 12:39:59 -0400

    change to yaml

--- src/main/java/io/camunda/cloudstarter/CloudStarterApplication.java
+++ src/main/java/io/camunda/cloudstarter/CloudStarterApplication.java
@@ -1,13 +1,11 @@
 package io.camunda.cloudstarter;
 
 import ch.qos.logback.classic.Logger;
-import io.zeebe.client.api.response.ActivatedJob;
-import io.zeebe.client.api.response.ProcessInstanceResult;
-import io.zeebe.client.api.response.Topology;
-import io.zeebe.spring.client.EnableZeebeClient;
-import io.zeebe.spring.client.ZeebeClientLifecycle;
-import io.zeebe.spring.client.annotation.ZeebeDeployment;
-import io.zeebe.spring.client.annotation.ZeebeWorker;
+import io.camunda.zeebe.client.api.response.Topology;
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.ZeebeClientLifecycle;
+import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -28,9 +26,13 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @ZeebeDeployment(classPathResources = "pricing-monitoring-2.bpmn")
 public class CloudStarterApplication {
-	@Autowired
-	private ZeebeClientLifecycle client;
+
+	private final ZeebeClientLifecycle client;
 	org.slf4j.Logger logger = LoggerFactory.getLogger(CloudStarterApplication.class);
+
+	public CloudStarterApplication(ZeebeClientLifecycle client) {
+		this.client = client;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStarterApplication.class, args);
@@ -43,6 +45,7 @@ public class CloudStarterApplication {
 		Topology topology = client.newTopologyRequest().send().join();
 		return topology.toString();
 	}
+
 //
 //	@Scheduled(cron = "${cron.expression}", zone = "${cron.zone}")
 //	public void startPricingWorkflow() throws InterruptedException {
